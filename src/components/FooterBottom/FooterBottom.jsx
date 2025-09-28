17af085c846bfe635939100ecbe184630ee032f4
Алексей <d44.a1@yandex.ru>
2025-09-28 20:08:58 +0300

    Add FooterBottom.jsx

---
+++ src/components/FooterBottom/FooterBottom.jsx
@@ -0,0 +1,13 @@
+import FooterCopyright from "../FooterCopyright/FooterCopyright";
+import FooterItem from "../FooterItem/FooterItem";
+
+function FooterBottom() {
+  return (
+    <div className="footer__copyright">
+      <FooterItem />
+      <FooterCopyright />
+    </div>
+  );
+}
+
+export default FooterBottom;

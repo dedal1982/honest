df716c249180247ad29c879561ac5d61d0c32fc0
Алексей <d44.a1@yandex.ru>
2025-09-28 17:24:51 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./components/App.jsx";
 import ErrorBoundaryWrapper from "./components/ErrorBoundary";
 
 createRoot(document.getElementById("root")).render(
